934e44b24edbc42ffb2cf1c3d54129efd0754770
Kayky Matos Santana <kayky7277@gmail.com>
2025-03-04 18:24:30 -0300

    find seller by id done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller=sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
